1424143e01301e8a3e0df08c2911b268d233e822
jaggedrose <jennierowe@hotmail.com>
2015-01-27 15:49:57 +0100

    Printing page list data to admin.html

--- js/main.js
+++ js/main.js
@@ -9,16 +9,27 @@ $(function() {
 			data: {
 				"search_param": search_param
 			},
-			success: printPages,
+			success: printPagesList,
 			error: function(data) {
 				console.log("getPages error: ", data);
 			}
 		});
 	}
 
-	function printPages(data) {
+	function printPagesList(data) {
 		console.log("printPages success: ", data);
-
+		$("#pageList table tr").not(".pageListHeader").remove();
+		// Get page data from DB and append to pageList
+		for (var i = 0; i < data.length; i++) {
+			var pageListInfo = $("<tr/>");
+			// Add page list info to page
+			pageListInfo.append('<td><span class="badge">'+data[i]["pid"]+"</span></td>");
+			pageListInfo.append('<td>'+data[i]["title"]+"</td>");
+			pageListInfo.append('<td>'+data[i]["author"]+"</td>");
+			pageListInfo.append('<td>'+data[i]["created"]+"</td>");
+			pageListInfo.append('<td>'+data[i]["updated"]+"</td>");
+			$("#pageList table").append(pageListInfo);
+		}
 	}
 
 	// Call needed function
--- php/classes/sqlqueries.class.php
+++ php/classes/sqlqueries.class.php
@@ -13,14 +13,14 @@ class SqlQueries extends PDOHelper {
 	// }
 
 	public function getAllPages() {
-		$sql2 = "SELECT * FROM pages";
-		return $this->query($sql2);
+		$sql = "SELECT pages.pid, pages.title, pages.body, pages.created, pages.updated, CONCAT(users.fname, ' ', users.lname) as author FROM pages, users";
+		return $this->query($sql);
 	}
 
 	// public function searchForPages($search_param) {
 	// 	$search_param = array(":search_param" => "%".$search_param."%");
-	// 	$sql3 = "SELECT * FROM pages WHERE title LIKE :search_param";
-	// 	return $this->query($sql3, $search_param);
+	// 	$sql = "SELECT * FROM pages WHERE title LIKE :search_param";
+	// 	return $this->query($sql, $search_param);
 	// }
 
 }
