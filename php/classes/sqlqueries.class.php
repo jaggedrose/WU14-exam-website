00f1cce7912906c9a92b3d736e2b642f8904565d
jaggedrose <jennierowe@hotmail.com>
2015-02-02 11:34:05 +0100

    sql query to get/add menu title, placement & mlid

--- php/classes/sqlqueries.class.php
+++ php/classes/sqlqueries.class.php
@@ -12,10 +12,19 @@ class SqlQueries extends PDOHelper {
 		//$sql1 = "INSERT INTO pages (title, body, user_id) VALUES (" . $page_data["title"]."," . $page_data["body"].",1)";
 		$sql1 = "INSERT INTO pages (title, body, user_id) VALUES (:title, :body, 1)";
 		return $this->query($sql1, $page_data);
+
+		$sql2 = "INSERT INTO menu_links (title, placement) VALUES (:title, :placement)";
+		return $this->query($sql2, $page_data);
+
 	}
 
 	public function getPages() {
 		$sql = "SELECT pages.pid, pages.title, pages.body, pages.created, CONCAT(users.fname, ' ', users.lname) as author FROM pages, users";
+		return $this->query($sql);
+	}
+
+	public function getMenus() {
+		$sql = "SELECT mlid, title, placement FROM menu_links";
 		return $this->query($sql);
 	}
 
--- php/get_menu_content.php
+++ php/get_menu_content.php
@@ -6,9 +6,9 @@ $queries = New SqlQueries("127.0.0.1","memade_magazine","root","mysql");
 
 // Get the menu links for a specific menu_name if told to do so 
 //(by receiving correct AJAX data), else get all menu_names
-if (isset($_REQUEST["menu_name"])) {
+if (isset($_REQUEST["menu_data"])) {
   // Get all menu_links for a specific menu_name
-  echo(json_encode($queries->getMenuLinks($_REQUEST["menu_name"])));
+  echo(json_encode($queries->getMenuLinks($_REQUEST["menu_data"])));
 } else {
   // Get all menu_names
   echo(json_encode($queries->getMenuNames()));
